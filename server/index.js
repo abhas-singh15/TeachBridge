5b7e21becc927726713bb24f5a3598ef927d0b3b
Abhas <kumar@Abhass-MacBook-Air.local>
2026-05-26 13:16:36 +0530

    Frontend url in CORS

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 8000;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-vercel-app.vercel.app"
+    "https://your-vercel-app.vercel.app",
+    "teach-bridge.vercel.app"
   ]
 }));
 
